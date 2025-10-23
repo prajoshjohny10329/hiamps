7af6fdd16579a54137cf72954c51c9dfde1faa0e
prajoshjohny10329 <prajoshjohny10329@gmail.com>
2025-10-23 13:44:34 +0530

    add footer

--- src/components/Checkout/Billing.tsx
+++ src/components/Checkout/Billing.tsx
@@ -187,7 +187,7 @@ const Billing = () => {
                       width="16"
                       height="16"
                       rx="4"
-                      fill="#3C50E0"
+                      fill="#ff0000"
                     />
                     <path
                       fillRule="evenodd"
