50129e94b21cce89b0cdf53cc225e2091b995f03
prajoshjohny10329 <prajoshjohny10329@gmail.com>
2025-10-30 17:27:40 +0530

    next is slider of trolly

--- src/components/Home/Hero/HeroCarousel.tsx
+++ src/components/Home/Hero/HeroCarousel.tsx
@@ -27,6 +27,170 @@ const HeroCarousal = () => {
         <div className="px-0 md:px-20 lg:px-30 flex items-center pt-6 sm:pt-0 flex-col sm:flex-row">
           {/* first part  */}
           <div className="w-full md:w-110   py-10 sm:py-15 lg:py-24.5 pl-4 sm:pl-7.5 lg:pl-12.5">
+            <h1 className="font-semibold text-black text-3xl  mb-1">
+              Hi-Amps {" "}
+              <span className="text-red-dark font-bold mb-3 text-3xl">
+                Inverters,
+              </span>
+            </h1>
+            <h2 className="font-semibold text-black text-3xl  mb-3">
+              Intelligent Power, Total Reliability
+            </h2>
+
+            <p className="text-black mt-7">
+              Built with <strong>DSP Sinewave Technology, heavy-duty transformers,</strong> and <strong>real-time LCD monitoring</strong>, Hi-Amps Inverters deliver clean, efficient, and safe power for every application. Designed to work seamlessly with Hi-Amps Tubular Batteries, they ensure unmatched backup performance and long service life.
+            </p>
+
+            <a
+              href="/products/inverter-ups"
+              className="inline-flex font-medium text-white text-custom-sm rounded-md bg-red-dark py-3 px-9 ease-out duration-200 hover:bg-red-dark mt-5"
+            >
+              Explore Hi-Amps Inverters
+            </a>
+          </div>
+          {/* second part  */}
+          <div className="relative flex-1 w-full">
+            <Image
+              src="/images/pages/products/hiamps-ups-inverter-1.webp"
+              alt="Hiamps UPS Inverter"
+              width={450}
+              height={550}
+              className="mx-auto max-w-full transition delay-150 duration-300 -z-10 contrast-125"
+            />
+            {/* Left Image */}
+            
+            <Image
+              src="/images/pages/products/hiamps-ups-inverter-1.webp"
+              alt="Hiamps UPS Inverter"
+              width={420}
+              height={550}
+              className="mx-auto max-w-full absolute -top-3 left-20 contrast-125 -z-1 blur-sm"
+            />
+            <Image
+              src="/images/pages/products/hiamps-ups-inverter-1.webp"
+              alt="Hiamps UPS Inverter"
+              width={420}
+              height={550}
+              className="mx-auto max-w-full absolute -top-5 right-20 contrast-125 -z-1 blur-sm"
+            />
+          </div>
+        </div>
+      </SwiperSlide>
+
+      <SwiperSlide>
+        {" "}
+        <div className="px-0 md:px-20 lg:px-30 flex items-center pt-6 sm:pt-0 flex-col sm:flex-row">
+          <div className="w-full md:w-110   py-10 sm:py-15 lg:py-24.5 pl-4 sm:pl-7.5 lg:pl-12.5">
+            <h1 className="font-semibold text-black text-3xl  mb-1">
+              Welcome To{" "}
+              <span className="text-red-dark font-bold mb-3 text-3xl">
+                Hiamps,
+              </span>
+            </h1>
+            <h2 className="font-semibold text-black text-3xl  mb-3">
+              Reliable Power Backup for Every Need
+            </h2>
+
+            <p className="text-black mt-7">
+              Choose from our advanced Hi-Amps{" "}
+              <span className="text-red font-bold">Red</span>,{" "}
+              <span className="text-green font-bold">Green</span>, and{" "}
+              <span className="text-blue font-bold">Blue</span> Series —
+              engineered for consistent performance, long life, and trusted
+              backup power for homes and offices.
+            </p>
+
+            <a
+              href="/products/power-backup-batteries"
+              className="inline-flex font-medium text-white text-custom-sm rounded-md bg-red-dark py-3 px-9 ease-out duration-200 hover:bg-red-dark mt-5"
+            >
+              Explore Power Backup Batteries
+            </a>
+          </div>
+          <div className="relative flex-1 w-full">
+            <Image
+              src="/images/pages/products/hiamps-blue-series-battery.webp"
+              alt="Hiamps Blue Series Battery"
+              width={450}
+              height={550}
+              className="mx-auto max-w-full transition delay-150 duration-300 -z-10 contrast-125"
+            />
+            <Image
+              src="/images/pages/products/hiamps-green-series-battery.webp"
+              alt="Hiamps Blue Series Battery"
+              width={420}
+              height={550}
+              className="mx-auto max-w-full absolute top-9 left-10 brightness-105 contrast-125"
+            />
+
+            <Image
+              src="/images/pages/products/hiamps-red-series-battery.webp"
+              alt="Hiamps Blue Series Battery"
+              width={420}
+              height={550}
+              className="mx-auto max-w-full absolute top-9 right-10 brightness-105 contrast-125"
+            />
+          </div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        {" "}
+        <div className="px-0 md:px-20 lg:px-30 flex items-center pt-6 sm:pt-0 flex-col sm:flex-row">
+          {/* first part  */}
+          <div className="w-full md:w-110   py-10 sm:py-15 lg:py-24.5 pl-4 sm:pl-7.5 lg:pl-12.5">
+            <h1 className="font-semibold text-black text-3xl  mb-1">
+              Hi-Amps {" "}
+              <span className="text-red-dark font-bold mb-3 text-3xl">
+                Inverters,
+              </span>
+            </h1>
+            <h2 className="font-semibold text-black text-3xl  mb-3">
+              Intelligent Power, Total Reliability
+            </h2>
+
+            <p className="text-black mt-7">
+              Built with <strong>DSP Sinewave Technology, heavy-duty transformers,</strong> and <strong>real-time LCD monitoring</strong>, Hi-Amps Inverters deliver clean, efficient, and safe power for every application. Designed to work seamlessly with Hi-Amps Tubular Batteries, they ensure unmatched backup performance and long service life.
+            </p>
+
+            <a
+              href="/products/inverter-ups"
+              className="inline-flex font-medium text-white text-custom-sm rounded-md bg-red-dark py-3 px-9 ease-out duration-200 hover:bg-red-dark mt-5"
+            >
+              Explore Hi-Amps Inverters
+            </a>
+          </div>
+          {/* second part  */}
+          <div className="relative flex-1 w-full">
+            <Image
+              src="/images/pages/products/hiamps-ups-inverter-1.webp"
+              alt="Hiamps UPS Inverter"
+              width={450}
+              height={550}
+              className="mx-auto max-w-full transition delay-150 duration-300 -z-10 contrast-125"
+            />
+            {/* Left Image */}
+            
+            <Image
+              src="/images/pages/products/hiamps-ups-inverter-1.webp"
+              alt="Hiamps UPS Inverter"
+              width={420}
+              height={550}
+              className="mx-auto max-w-full absolute -top-3 left-20 contrast-125 -z-1 blur-sm"
+            />
+            <Image
+              src="/images/pages/products/hiamps-ups-inverter-1.webp"
+              alt="Hiamps UPS Inverter"
+              width={420}
+              height={550}
+              className="mx-auto max-w-full absolute -top-5 right-20 contrast-125 -z-1 blur-sm"
+            />
+          </div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        {" "}
+        <div className="px-0 md:px-20 lg:px-30 flex items-center pt-6 sm:pt-0 flex-col sm:flex-row">
+          <div className="w-full md:w-110   py-10 sm:py-15 lg:py-24.5 pl-4 sm:pl-7.5 lg:pl-12.5">
             
             <h1 className="font-semibold text-black text-3xl  mb-1">Welcome To <span className="text-red-dark font-bold mb-3 text-3xl">Hiamps</span></h1>
             <h2 className="font-semibold text-black text-3xl  mb-3">
@@ -44,111 +208,25 @@ const HeroCarousal = () => {
               View All Our Products
             </a>
           </div>
-          {/* second part  */}
           <div className="relative flex-1 w-full">
             <Image
               src="/images/pages/products/hi-amps-all-products.webp"
               alt="headphone"
               width={1000}
               height={1000}
-              className="w-full h-auto ml-3"
+              className="w-full h-auto ml-3  contrast-125"
             />
             <Image
               src="/images/pages/products/hi-amps-all-products.webp"
               alt="headphone"
               width={1000}
               height={1000}
-              className="w-full h-auto absolute left-2 -z-1 md:left-10 bottom-0"
+              className="w-full h-auto absolute left-2 -z-1 md:left-10 bottom-0 brightness-105 contrast-125"
             />
             
           </div>
-
         </div>
       </SwiperSlide>
-      {/* <SwiperSlide>
-        {" "}
-        <div className="flex items-center pt-6 sm:pt-0 flex-col-reverse sm:flex-row">
-          <div className="max-w-[394px] py-10 sm:py-15 lg:py-26 pl-4 sm:pl-7.5 lg:pl-12.5">
-            <div className="flex items-center gap-4 mb-7.5 sm:mb-10">
-              <span className="block font-semibold text-heading-3 sm:text-heading-1 text-blue">
-                30%
-              </span>
-              <span className="block text-dark text-sm sm:text-custom-1 sm:leading-[24px]">
-                Sale
-                <br />
-                Off
-              </span>
-            </div>
-
-            <h1 className="font-semibold text-dark text-xl sm:text-3xl mb-3">
-              <a href="#">True Wireless Noise Cancelling Headphone</a>
-            </h1>
-
-            <p>
-              Lorem ipsum dolor sit, consectetur elit nunc suscipit non ipsum
-              nec suscipit.
-            </p>
-
-            <a
-              href="#"
-              className="inline-flex font-medium text-white text-custom-sm rounded-md bg-dark py-3 px-9 ease-out duration-200 hover:bg-red-dark mt-10"
-            >
-              Shop Now
-            </a>
-          </div>
-
-          <div>
-            <Image
-              src="/images/hero/hero-01.png"
-              alt="headphone"
-              width={351}
-              height={358}
-            />
-          </div>
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        {" "}
-        <div className="flex items-center pt-6 sm:pt-0 flex-col-reverse sm:flex-row">
-          <div className="max-w-[394px] py-10 sm:py-15 lg:py-26 pl-4 sm:pl-7.5 lg:pl-12.5">
-            <div className="flex items-center gap-4 mb-7.5 sm:mb-10">
-              <span className="block font-semibold text-heading-3 sm:text-heading-1 text-blue">
-                30%
-              </span>
-              <span className="block text-dark text-sm sm:text-custom-1 sm:leading-[24px]">
-                Sale
-                <br />
-                Off
-              </span>
-            </div>
-
-            <h1 className="font-semibold text-dark text-xl sm:text-3xl mb-3">
-              <a href="#">True Wireless Noise Cancelling Headphone</a>
-            </h1>
-
-            <p>
-              Lorem ipsum dolor sit, consectetur elit nunc suscipit non ipsum
-              nec suscipit.
-            </p>
-
-            <a
-              href="#"
-              className="inline-flex font-medium text-white text-custom-sm rounded-md bg-dark py-3 px-9 ease-out duration-200 hover:bg-red-dark mt-10"
-            >
-              Shop Now
-            </a>
-          </div>
-
-          <div>
-            <Image
-              src="/images/hero/hero-01.png"
-              alt="headphone"
-              width={351}
-              height={358}
-            />
-          </div>
-        </div>
-      </SwiperSlide> */}
     </Swiper>
   );
 };
