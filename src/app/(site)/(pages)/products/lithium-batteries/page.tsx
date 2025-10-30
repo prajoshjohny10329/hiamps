e2568663d44c4a12fab808aa44fad57f8bf184f1
prajoshjohny10329 <prajoshjohny10329@gmail.com>
2025-10-30 13:45:31 +0530

    edi befor homw

---
+++ src/app/(site)/(pages)/products/lithium-batteries/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+import Breadcrumb from "@/components/Common/Breadcrumb";
+import Image from "next/image";
+
+export default function PowerBackupBatteries() {
+  return (
+    <section>
+      <Breadcrumb
+        title={"Lithium Batteries"}
+        pages={["Lithium Batteries"]}
+      />
+
+      <section className="mx-auto max-w-[1390px] px-4 md:px-8 xl:px-9">
+        <h1 className="my-[100px] text-3xl font-semibold text-center  text-black">This Page Will Coming Soon</h1>
+      </section>
+
+    </section>
+  );
+}
