ef755e21122ae6af46c75177f413b1e62ae89819
prajoshjohny10329 <prajoshjohny10329@gmail.com>
2025-10-30 16:01:44 +0530

    add first slider

--- src/components/Home/Hero/HeroCarousel.tsx
+++ src/components/Home/Hero/HeroCarousel.tsx
@@ -24,46 +24,48 @@ const HeroCarousal = () => {
       className="hero-carousel"
     >
       <SwiperSlide>
-        <div className="flex items-center pt-6 sm:pt-0 flex-col-reverse sm:flex-row">
-          <div className="max-w-[394px] py-10 sm:py-15 lg:py-24.5 pl-4 sm:pl-7.5 lg:pl-12.5">
-            <div className="flex items-center gap-4 mb-7.5 sm:mb-10">
-              <span className="block font-semibold text-heading-3 sm:text-heading-1 text-blue">
-                30%
-              </span>
-              <span className="block text-dark text-sm sm:text-custom-1 sm:leading-[24px]">
-                Sale
-                <br />
-                Off
-              </span>
-            </div>
+        <div className="px-0 md:px-20 lg:px-30 flex items-center pt-6 sm:pt-0 flex-col sm:flex-row">
+          {/* first part  */}
+          <div className="w-full md:w-110   py-10 sm:py-15 lg:py-24.5 pl-4 sm:pl-7.5 lg:pl-12.5">
+            
+            <h1 className="font-semibold text-black text-3xl  mb-1">Welcome To <span className="text-red-dark font-bold mb-3 text-3xl">Hiamps</span></h1>
+            <h2 className="font-semibold text-black text-3xl  mb-3">
+              Explore Our Complete Range of Power Solutions
+            </h2>
 
-            <h1 className="font-semibold text-dark text-xl sm:text-3xl mb-3">
-              <a href="#">True Wireless Noise Cancelling Headphone</a>
-            </h1>
-
-            <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi at ipsum at risus euismod lobortis in
+            <p className="text-black mt-7">
+            From high-performance tubular batteries to advanced inverters — find the perfect power solution designed for your home, office, or industry.
             </p>
 
             <a
-              href="#"
-              className="inline-flex font-medium text-white text-custom-sm rounded-md bg-dark py-3 px-9 ease-out duration-200 hover:bg-red-dark mt-10"
+              href="/products"
+              className="inline-flex font-medium text-white text-custom-sm rounded-md bg-red-dark py-3 px-9 ease-out duration-200 hover:bg-red-dark mt-5"
             >
-              Shop Now
+              View All Our Products
             </a>
           </div>
-
-          <div>
+          {/* second part  */}
+          <div className="relative flex-1 w-full">
             <Image
-              src="/images/pages/slider-1.webp"
+              src="/images/pages/products/hi-amps-all-products.webp"
               alt="headphone"
-              width={351}
-              height={358}
+              width={1000}
+              height={1000}
+              className="w-full h-auto ml-3"
             />
+            <Image
+              src="/images/pages/products/hi-amps-all-products.webp"
+              alt="headphone"
+              width={1000}
+              height={1000}
+              className="w-full h-auto absolute left-2 -z-1 md:left-10 bottom-0"
+            />
+            
           </div>
+
         </div>
       </SwiperSlide>
-      <SwiperSlide>
+      {/* <SwiperSlide>
         {" "}
         <div className="flex items-center pt-6 sm:pt-0 flex-col-reverse sm:flex-row">
           <div className="max-w-[394px] py-10 sm:py-15 lg:py-26 pl-4 sm:pl-7.5 lg:pl-12.5">
@@ -146,7 +148,7 @@ const HeroCarousal = () => {
             />
           </div>
         </div>
-      </SwiperSlide>
+      </SwiperSlide> */}
     </Swiper>
   );
 };
